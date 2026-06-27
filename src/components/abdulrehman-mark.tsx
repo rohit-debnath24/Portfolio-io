682e97a6b092c2043d7726c18a3f01df4ffbbd3f
Rohit Debnath <rohit.kumardebnath.cse28@heritageit.edu.in>
2026-06-27 23:26:00 +0530

    changes/2

--- src/components/abdulrehman-mark.tsx
+++ src/components/abdulrehman-mark.tsx
@@ -9,17 +9,15 @@ export function AbdulRehmanMark(props: React.ComponentProps<"svg">) {
       <path
         fill="currentColor"
         d="
-            M48 0h96v64H48V0Z
-            M0 64h64v64H0V64Z
-            M128 64h64v64h-64V64Z
-            M0 128h192v64H0v-64Z
-            M0 192h64v64H0v-64Z
-            M128 192h64v64h-64v-64Z
+            M0 0h64v256H0V0Z
+            M64 0h128v64H64V0Z
+            M192 64h64v64h-64V64Z
+            M64 128h128v64H64v-64Z
+            M192 192h64v64h-64v-64Z
             M256 0h64v256h-64V0Z
             M320 0h128v64H320V0Z
-            M448 64h64v64h-64V64Z
-            M320 128h128v64H320v-64Z
-            M448 192h64v64h-64v-64Z
+            M448 64h64v128h-64V64Z
+            M320 192h128v64H320v-64Z
           "
       ></path>
     </svg>
@@ -27,5 +25,5 @@ export function AbdulRehmanMark(props: React.ComponentProps<"svg">) {
 }
 
 export function getMarkSVG(color: string) {
-  return `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 256 128"><path fill="${color}" d="M96 128H32V96h64v32ZM224 32h-64v64h64v32h-96V0h96v32ZM32 96H0V32h32v64ZM256 96h-32V32h32v64ZM96 32H32V0h64v32Z"/></svg>`;
+  return `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 256 128"><path fill="${color}" d="M0 0h32v128H0V0ZM32 0h64v32H32V0ZM96 32h32v32H96V32ZM32 64h64v32H32V64ZM96 96h32v32H96V96ZM128 0h32v128H128V0ZM160 0h64v32H160V0ZM224 32h32v64H224V32ZM160 96h64v32H160V96Z"/></svg>`;
 }
